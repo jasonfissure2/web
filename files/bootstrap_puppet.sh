b13a3a2acd77206cce4386d366de5122a95f81b7
jfisher <jfisher@modopayments.com>
2018-04-03 14:58:29 -0500

    fixes for out of date puppet and nginx install info

--- files/bootstrap_puppet.sh
+++ files/bootstrap_puppet.sh
@@ -2,8 +2,8 @@
 sudo curl https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb -o puppetlabs-release.deb
 sudo dpkg -i puppetlabs-release.deb
 sudo apt-get -qq update
-sudo apt-get install -yq puppet-agent
-sudo /opt/puppetlabs/bin/puppet module install puppet-nginx
+sudo apt-get install -yq puppet-agent=1.8.3-1xenial
+sudo /opt/puppetlabs/bin/puppet module install puppet-nginx --version 0.5.0
 cat >/tmp/nginx.pp << "EOF"
 class{'nginx': }
 nginx::resource::vhost{'www.example.com':
